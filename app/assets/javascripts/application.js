74328031b88b7def3aa3da67f0cda7f006e957a1
Kyle Swaffield <kyle@swaff.id.au>
2020-12-07 21:42:57 +1100

    monday-2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,11 @@
 //
 //= require rails-ujs
 //= require_tree .
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
